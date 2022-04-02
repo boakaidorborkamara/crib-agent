17ef3d2223b56eef303c31cd9d8b81879dae4029
Boakai <boakaidkamara@gmail.com>
2022-04-02 20:29:07 +0000

    recieving and responding to messages from messenger inbox

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
 
+// Enable request to facebook server 
+const request = require('request');
+
 
 
 
@@ -26,7 +29,68 @@ app.get("/",(req,res)=>{
 app.post('/webhook', (req, res) => {  
  
     let body = req.body;
+
+
+    // Handles messages events
+    function handleMessage(sender_psid, received_message) {
+      let response;
+
+      // Check if the message contains text
+      if (received_message.text) {    
+    
+        // Create the payload for a basic text message
+        response = {
+          "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+      }  
+      
+      // Sends the response message
+      callSendAPI(sender_psid, response);
+    }
+
+    // Handles messaging_postbacks events
+    function handlePostback(sender_psid, received_postback) {
+      let response;
   
+      // Get the payload for the postback
+      let payload = received_postback.payload;
+
+      // Set the response based on the postback payload
+      if (payload === 'yes') {
+        response = { "text": "Thanks!" }
+      } else if (payload === 'no') {
+        response = { "text": "Oops, try sending another image." }
+      }
+      // Send the message to acknowledge the postback
+      callSendAPI(sender_psid, response);
+    }
+
+    // Sends response messages via the Send API
+    function callSendAPI(sender_psid, response) {
+      // Construct the message body
+      let request_body = {
+        "recipient": {
+          "id": sender_psid
+        },
+        "message": response
+      }
+
+      // Send the HTTP request to the Messenger Platform
+      request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": process.env.TOKEN },
+        "method": "POST",
+        "json": request_body
+      }, (err, res, body) => {
+        if (!err) {
+          console.log('message sent!')
+        } else {
+          console.error("Unable to send message:" + err);
+        }
+      });
+    }
+  
+
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
   
@@ -41,6 +105,14 @@ app.post('/webhook', (req, res) => {
         //Get Sender PSID
         let sender_psid = webhook_event.sender.id;
         console.log('Sender PSID: ' + sender_psid);
+
+        // Check if the event is a message or postback and
+        // pass the event to the appropriate handler function
+        if (webhook_event.message) {
+          handleMessage(sender_psid, webhook_event.message);        
+        } else if (webhook_event.postback) {
+          handlePostback(sender_psid, webhook_event.postback);
+        }
       });
   
       // Returns a '200 OK' response to all requests
